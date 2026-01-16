c0ce55cf5c2604f38d2009182a4139a47a3dd63f
Kaoutar laajil <129660727+kooutar@users.noreply.github.com>
2026-01-16 10:06:00 +0100

    initiate the project

---
+++ src/main/java/com/example/ModeConnect/ModeConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ModeConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ModeConnect/ModeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ModeConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
